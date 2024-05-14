e1fee1d07e2a800231c6ac4d44157b30b5c2ceb9
jkotor <jkotor@gmail.com>
2024-05-14 15:34:47 +0200

    Fix failing gradle resolutions - by adding compose.bom dep manually

--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -7,6 +7,7 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
