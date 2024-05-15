70c820482676fe50c93bc60d1653c294e7f6e428
jkotor <jkotor@gmail.com>
2024-05-15 16:31:51 +0200

    Remove unnecessary deps from location gradle

--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -7,16 +7,9 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.material3)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.google.android.gms.play.services.location)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
 
     implementation(projects.core.domain)
     implementation(projects.run.domain)
