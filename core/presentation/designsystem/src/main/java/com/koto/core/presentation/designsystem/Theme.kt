4388d4c437481def10a968017edf5ad389fb8b58
jkotor <jkotor@gmail.com>
2024-05-15 16:35:10 +0200

    Remove redundant imports

--- core/presentation/designsystem/src/main/java/com/koto/core/presentation/designsystem/Theme.kt
+++ core/presentation/designsystem/src/main/java/com/koto/core/presentation/designsystem/Theme.kt
@@ -1,12 +1,10 @@
 package com.koto.core.presentation.designsystem
 
 import android.app.Activity
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
