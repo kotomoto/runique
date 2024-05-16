22d47bd961ec09dc7c6784917b7f2d362bfe46ff
jkotor <jkotor@gmail.com>
2024-05-16 15:48:23 +0200

    Update http client extensions

--- core/data/src/main/java/com/koto/core/data/networking/HttpClientExt.kt
+++ core/data/src/main/java/com/koto/core/data/networking/HttpClientExt.kt
@@ -3,22 +3,79 @@ package com.koto.core.data.networking
 import com.koto.core.data.BuildConfig
 import com.koto.core.domain.util.DataError
 import com.koto.core.domain.util.Result
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.CancellationException
+import kotlinx.serialization.SerializationException
 
-// todo
-//suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): Result<T, DataError.Network> {
-//    val response = try {
-//        execute()
-//    } catch (e: UnresolvedAddressException) {
-//        e.printStackTrace()
-//        Result.Error(DataError.Network.NO_INTERNET)
-//    }
-//}
+suspend inline fun <reified Response : Any> HttpClient.get(
+    route: String,
+    queryParameters: Map<String, Any?> = mapOf(),
+): Result<Response, DataError.Network> {
+    return safeCall {
+        get {
+            url(constructRoute(route))
+            queryParameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+}
+
+suspend inline fun <reified Response : Any> HttpClient.delete(
+    route: String,
+    queryParameters: Map<String, Any?> = mapOf(),
+): Result<Response, DataError.Network> {
+    return safeCall {
+        delete {
+            url(constructRoute(route))
+            queryParameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+}
+
+suspend inline fun <reified Request, reified Response : Any> HttpClient.post(
+    route: String,
+    body: Request,
+): Result<Response, DataError.Network> {
+    return safeCall {
+        post {
+            url(constructRoute(route))
+            setBody(body)
+        }
+    }
+}
+
+suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): Result<T, DataError.Network> {
+    val response = try {
+        execute()
+    } catch (e: UnresolvedAddressException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.NO_INTERNET)
+    } catch (e: SerializationException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.SERIALIZATION)
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e
+        e.printStackTrace()
+        return Result.Error(DataError.Network.UNKNOWN)
+    }
+
+    return responseToResult(response)
+}
 
 suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Network> {
-    return when(response.status.value) {
+    return when (response.status.value) {
         in 200..299 -> Result.Success(response.body<T>())
         401 -> Result.Error(DataError.Network.UNAUTHORIZED)
         408 -> Result.Error(DataError.Network.REQUEST_TIMEOUT)
