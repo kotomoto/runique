717306dbdd4eb0bed93f86641b0ea54d059b40cf
jkotor <jkotor@gmail.com>
2024-05-21 00:23:10 +0200

    Add password field and register button to Register screen

--- auth/presentation/src/main/java/com/koto/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/koto/auth/presentation/register/RegisterScreen.kt
@@ -4,17 +4,21 @@ package com.koto.auth.presentation.register
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -24,13 +28,21 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.koto.auth.domain.PasswordValidationState
+import com.koto.auth.domain.UserDataValidator
 import com.koto.auth.presentation.R
 import com.koto.core.presentation.designsystem.CheckIcon
+import com.koto.core.presentation.designsystem.CrossIcon
 import com.koto.core.presentation.designsystem.EmailIcon
 import com.koto.core.presentation.designsystem.Poppins
+import com.koto.core.presentation.designsystem.RuniqueDarkRed
 import com.koto.core.presentation.designsystem.RuniqueGray
+import com.koto.core.presentation.designsystem.RuniqueGreen
 import com.koto.core.presentation.designsystem.RuniqueTheme
 import com.koto.core.presentation.designsystem.components.GradientBackground
+import com.koto.core.presentation.designsystem.components.RuniqueActionButton
+import com.koto.core.presentation.designsystem.components.RuniquePasswordTextField
 import com.koto.core.presentation.designsystem.components.RuniqueTextField
 import org.koin.androidx.compose.koinViewModel
 
@@ -103,16 +115,80 @@ private fun RegisterScreen(
             RuniqueTextField(
                 state = state.email,
                 startIcon = EmailIcon,
-                endIcon = if (state.isEmailValid) {
-                    CheckIcon
-                } else null,
+                endIcon = if (state.isEmailValid) CheckIcon else null,
                 hint = stringResource(id = R.string.example_email),
                 title = stringResource(id = R.string.email),
                 modifier = Modifier.fillMaxWidth(),
                 additionalInfo = stringResource(id = R.string.must_be_a_valid_email),
                 keyboardType = KeyboardType.Email,
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            RuniquePasswordTextField(
+                state = state.password,
+                isPasswordVisible = state.isPasswordVisible,
+                onTogglePasswordVisibility = { onAction(RegisterAction.OnTogglePasswordVisibilityClick) },
+                hint = stringResource(id = R.string.password),
+                title = stringResource(id = R.string.password),
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    id = R.string.at_least_x_characters,
+                    UserDataValidator.MIN_PASSWORD_LENGTH,
+                ),
+                isValid = state.passwordValidationState.hasMinLength,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(id = R.string.at_least_one_number),
+                isValid = state.passwordValidationState.hasNumber,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(id = R.string.contains_lowercase_char),
+                isValid = state.passwordValidationState.hasLowercaseCharacter,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(id = R.string.contains_uppercase_char),
+                isValid = state.passwordValidationState.hasUppercaseCharacter,
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            RuniqueActionButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.register),
+                isLoading = state.isRegistering,
+                enabled = state.canRegister,
+                onClick = {
+                    onAction(RegisterAction.OnRegisterClick)
+                }
+            )
         }
+    }
+}
+
+@Composable
+fun PasswordRequirement(
+    text: String,
+    isValid: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = if (isValid) CheckIcon else CrossIcon,
+            contentDescription = null,
+            tint = if (isValid) RuniqueGreen else RuniqueDarkRed
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontSize = 14.sp,
+        )
     }
 }
 
@@ -121,7 +197,11 @@ private fun RegisterScreen(
 private fun RegisterScreenPreview() {
     RuniqueTheme {
         RegisterScreen(
-            state = RegisterState(),
+            state = RegisterState(
+                passwordValidationState = PasswordValidationState(
+                    hasNumber = true,
+                )
+            ),
             onAction = {},
         )
     }
