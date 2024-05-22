5d2fc72d2a4bc2dfd9fd4877dbc47971e97f5a97
jkotor <jkotor@gmail.com>
2024-05-22 12:59:17 +0200

    Clear appModule to be filled later

--- app/src/main/java/com/koto/runique/di/AppModule.kt
+++ app/src/main/java/com/koto/runique/di/AppModule.kt
@@ -1,15 +1,7 @@
 package com.koto.runique.di
 
-import com.koto.auth.data.EmailPatternValidator
-import com.koto.auth.domain.PatternValidator
-import com.koto.auth.domain.UserDataValidator
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule = module {
-    single<PatternValidator> {
-        EmailPatternValidator
-    }
-
-    singleOf(::UserDataValidator)
+    // todo
 }
