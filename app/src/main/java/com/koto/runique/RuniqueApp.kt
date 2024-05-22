4c609ecb5a60bf04405c9bd812fa23f1b2292c11
jkotor <jkotor@gmail.com>
2024-05-22 12:59:31 +0200

    Create application class

---
+++ app/src/main/java/com/koto/runique/RuniqueApp.kt
@@ -0,0 +1,31 @@
+package com.koto.runique
+
+import android.app.Application
+import com.koto.auth.data.di.authDataModule
+import com.koto.auth.presentation.di.authViewModelModule
+import com.koto.runique.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class RuniqueApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        startKoin {
+            androidLogger()
+            androidContext(this@RuniqueApp)
+            modules(
+                authDataModule,
+                authViewModelModule,
+                appModule,
+            )
+        }
+    }
+}
