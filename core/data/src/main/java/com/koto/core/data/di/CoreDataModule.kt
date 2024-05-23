fd7a73ed9714bbb1270cb9dfaa0b3e277a972d81
jkotor <jkotor@gmail.com>
2024-05-23 12:36:36 +0200

    Set up encrypted session storage

--- core/data/src/main/java/com/koto/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/koto/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.koto.core.data.di
 
+import com.koto.core.data.auth.EncryptedSessionStorage
 import com.koto.core.data.networking.HttpClientFactory
+import com.koto.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
---
+++ core/domain/src/main/java/com/koto/core/domain/SessionStorage.kt
@@ -0,0 +1,6 @@
+package com.koto.core.domain
+
+interface SessionStorage {
+    suspend fun get(): AuthInfo?
+    suspend fun set(info: AuthInfo?)
+}
