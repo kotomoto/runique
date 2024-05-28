2957f92b07bd0f2476d63f8b8ab6a1260ca9c468
jkotor <jkotor@gmail.com>
2024-05-28 11:44:51 +0200

    Add toolbar with actions

--- core/presentation/designsystem/src/main/java/com/koto/core/presentation/designsystem/components/RuniqueToolbar.kt
+++ core/presentation/designsystem/src/main/java/com/koto/core/presentation/designsystem/components/RuniqueToolbar.kt
@@ -2,11 +2,17 @@
 
 package com.koto.core.presentation.designsystem.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -27,6 +33,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.koto.core.presentation.designsystem.AnalyticsIcon
 import com.koto.core.presentation.designsystem.ArrowLeftIcon
 import com.koto.core.presentation.designsystem.LogoIcon
 import com.koto.core.presentation.designsystem.Poppins
@@ -80,6 +87,44 @@ fun RuniqueToolbar(
                     )
                 }
             }
+        },
+        actions = {
+            if (menuItems.isNotEmpty()) {
+                Box {
+                    DropdownMenu(
+                        expanded = isDropDownOpen,
+                        onDismissRequest = {
+                            isDropDownOpen = false
+                        }
+                    ) {
+                        menuItems.forEachIndexed { index, item ->
+                            Row(
+                                modifier = Modifier
+                                    .clickable { onMenuItemClick(index) }
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.title,
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(text = item.title)
+                            }
+                        }
+                    }
+                    IconButton(onClick = {
+                        isDropDownOpen = true
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = stringResource(id = R.string.open_menu),
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                }
+            }
         }
     )
 }
@@ -90,7 +135,7 @@ private fun RuniqueToolbarPreview() {
     RuniqueTheme {
         RuniqueToolbar(
             modifier = Modifier.fillMaxWidth(),
-            showBackButton = false,
+            showBackButton = true,
             title = "Runique",
             startContent = {
                 Icon(
@@ -99,7 +144,13 @@ private fun RuniqueToolbarPreview() {
                     tint = RuniqueGreen,
                     modifier = Modifier.size(35.dp)
                 )
-            }
+            },
+            menuItems = listOf(
+                DropDownItem(
+                    icon = AnalyticsIcon,
+                    title = "Analytics",
+                )
+            )
         )
     }
 }
