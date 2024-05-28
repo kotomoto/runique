97bc0bf0df96dbfcff8910317963cc55b9e9dc0f
jkotor <jkotor@gmail.com>
2024-05-28 12:42:52 +0200

    Add Run overview screen

--- app/src/main/java/com/koto/runique/RuniqueApp.kt
+++ app/src/main/java/com/koto/runique/RuniqueApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.koto.auth.data.di.authDataModule
 import com.koto.auth.presentation.di.authViewModelModule
 import com.koto.core.data.di.coreDataModule
+import com.koto.run.presentation.di.runViewModelModule
 import com.koto.runique.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -27,6 +28,7 @@ class RuniqueApp : Application() {
                 authViewModelModule,
                 appModule,
                 coreDataModule,
+                runViewModelModule,
             )
         }
     }
--- run/presentation/src/main/java/com/koto/run/presentation/run_overview/RunOverviewScreen.kt
+++ run/presentation/src/main/java/com/koto/run/presentation/run_overview/RunOverviewScreen.kt
@@ -1,8 +1,28 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.koto.run.presentation.run_overview
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.koto.core.presentation.designsystem.AnalyticsIcon
+import com.koto.core.presentation.designsystem.LogoIcon
+import com.koto.core.presentation.designsystem.LogoutIcon
+import com.koto.core.presentation.designsystem.RunIcon
 import com.koto.core.presentation.designsystem.RuniqueTheme
+import com.koto.core.presentation.designsystem.components.RuniqueFloatingActionButton
+import com.koto.core.presentation.designsystem.components.RuniqueScaffold
+import com.koto.core.presentation.designsystem.components.RuniqueToolbar
+import com.koto.core.presentation.designsystem.components.util.DropDownItem
+import com.koto.run.presentation.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -19,7 +39,59 @@ fun RunOverviewScreenRoot(
 private fun RunOverviewScreen(
     onAction: (RunOverviewAction) -> Unit
 ) {
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        state = topAppBarState
+    )
 
+    RuniqueScaffold(
+        topAppBar = {
+            RuniqueToolbar(
+                showBackButton = false,
+                title = stringResource(id = R.string.runique),
+                scrollBehavior = scrollBehavior,
+                menuItems = listOf(
+                    DropDownItem(
+                        icon = AnalyticsIcon,
+                        title = stringResource(id = R.string.analytics),
+                    ),
+                    DropDownItem(
+                        icon = LogoutIcon,
+                        title = stringResource(id = R.string.logout),
+                    )
+                ),
+                onMenuItemClick = { index ->
+                    when (index) {
+                        0 -> {
+                            onAction(RunOverviewAction.OnAnalyticsClick)
+                        }
+
+                        1 -> {
+                            onAction(RunOverviewAction.OnLogoutClick)
+                        }
+                    }
+                },
+                startContent = {
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        imageVector = LogoIcon,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            RuniqueFloatingActionButton(
+                icon = RunIcon,
+                onClick = {
+                    onAction(RunOverviewAction.OnStartClick)
+                }
+            )
+        }
+    ) { padding ->
+
+    }
 }
 
 @Preview
