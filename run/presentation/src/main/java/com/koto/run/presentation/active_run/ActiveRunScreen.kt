6264d712ec9570c1059c61c52d2750ded3be26be
jkotor <jkotor@gmail.com>
2024-05-29 14:43:17 +0200

    WIP - Handle permissions
    Location and notification permissions

--- run/presentation/src/main/java/com/koto/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/koto/run/presentation/active_run/ActiveRunScreen.kt
@@ -1,7 +1,12 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package com.koto.run.presentation.active_run
 
+import android.Manifest
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +16,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,6 +28,8 @@ import com.koto.core.presentation.designsystem.components.RuniqueScaffold
 import com.koto.core.presentation.designsystem.components.RuniqueToolbar
 import com.koto.run.presentation.R
 import com.koto.run.presentation.active_run.components.RunDataCard
+import com.koto.run.presentation.util.shouldShowLocationPermissionRationale
+import com.koto.run.presentation.util.shouldShowNotificationPermissionRationale
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -40,6 +48,37 @@ private fun ActiveRunScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    val context = LocalContext.current
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { perms ->
+        val hasCoarseLocationPermission = perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+            perms[Manifest.permission.POST_NOTIFICATIONS] == true
+        } else {
+            true
+        }
+
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = hasCoarseLocationPermission && hasFineLocationPermission,
+                showLocationRationale = showLocationRationale,
+            )
+        )
+
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = hasNotificationPermission,
+                showNotificationRationale = showNotificationRationale,
+            )
+        )
+    }
+
     RuniqueScaffold(
         withGradient = false,
         topAppBar = {
