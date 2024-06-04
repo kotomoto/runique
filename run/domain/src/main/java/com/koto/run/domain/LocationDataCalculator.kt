ae41d0efc14697b3868544299495ab188923926d
jkotor <jkotor@gmail.com>
2024-06-04 17:10:22 +0200

    Implement RunningTracker internals

---
+++ run/domain/src/main/java/com/koto/run/domain/LocationDataCalculator.kt
@@ -0,0 +1,18 @@
+package com.koto.run.domain
+
+import com.koto.core.domain.location.LocationTimestamp
+import kotlin.math.roundToInt
+
+object LocationDataCalculator {
+
+    fun getTotalDistanceMeters(locations: List<List<LocationTimestamp>>): Int {
+        return locations
+            .sumOf { timestampsPerLine ->
+                timestampsPerLine.zipWithNext { location1, location2 ->
+                    location1.location.location.distanceTo(location2.location.location)
+                }.sum().roundToInt()
+            }
+    }
+
+
+}
