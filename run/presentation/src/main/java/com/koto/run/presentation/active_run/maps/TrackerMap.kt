7e04ca765bfe5e12c2db38142c15cc8f0e7d4c76
jkotor <jkotor@gmail.com>
2024-06-06 16:20:15 +0200

    Draw polylines on a map

--- run/presentation/src/main/java/com/koto/run/presentation/active_run/maps/TrackerMap.kt
+++ run/presentation/src/main/java/com/koto/run/presentation/active_run/maps/TrackerMap.kt
@@ -92,6 +92,8 @@ fun TrackerMap(
             zoomControlsEnabled = false,
         ),
     ) {
+        RuniquePolylines(locations = locations)
+
         if (!isRunFinished && currentLocation != null) {
             MarkerComposable(
                 currentLocation,
