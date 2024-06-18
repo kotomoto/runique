5eaf1ff1db5a85e88943a8bde6bf53b9a2a5d549
jkotor <jkotor@gmail.com>
2024-06-18 17:19:21 +0200

    WIP - add run item UI - part 2

--- run/presentation/src/main/java/com/koto/run/presentation/run_overview/components/RunListItem.kt
+++ run/presentation/src/main/java/com/koto/run/presentation/run_overview/components/RunListItem.kt
@@ -4,12 +4,24 @@ package com.koto.run.presentation.run_overview.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,14 +29,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
 import com.koto.core.domain.location.Location
 import com.koto.core.domain.run.Run
+import com.koto.core.presentation.designsystem.CalendarIcon
+import com.koto.core.presentation.designsystem.RunOutlinedIcon
 import com.koto.core.presentation.designsystem.RuniqueTheme
+import com.koto.run.presentation.R
 import com.koto.run.presentation.run_overview.mapper.toRunUi
 import com.koto.run.presentation.run_overview.model.RunDataUi
 import com.koto.run.presentation.run_overview.model.RunUi
@@ -52,8 +70,126 @@ fun RunListItem(
                 }
             )
             .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
+        MapImage(imageUrl = runUi.mapPictureUrl)
+        RunningTimeSection(
+            modifier = Modifier.fillMaxWidth(),
+            duration = runUi.duration,
+        )
+        HorizontalDivider(color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f))
+        RunningDateSection(
+            dateTime = runUi.dateTime,
+        )
+    }
+}
 
+@Composable
+private fun MapImage(
+    modifier: Modifier = Modifier,
+    imageUrl: String?,
+) {
+    SubcomposeAsyncImage(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(16 / 9f)
+            .clip(RoundedCornerShape(15.dp)),
+        model = imageUrl,
+        contentDescription = stringResource(id = R.string.run_map),
+        loading = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(20.dp),
+                    strokeWidth = 2.dp,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+            }
+        },
+        error = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.errorContainer),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.error_couldnt_load_image),
+                    color = MaterialTheme.colorScheme.error,
+                )
+            }
+        }
+    )
+}
+
+@Composable
+private fun RunningTimeSection(
+    modifier: Modifier = Modifier,
+    duration: String,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f))
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(4.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                imageVector = RunOutlinedIcon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+            )
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            modifier = Modifier
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = stringResource(id = R.string.total_running_time),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+            )
+            Text(
+                text = duration,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+        }
+    }
+}
+
+@Composable
+private fun RunningDateSection(
+    modifier: Modifier = Modifier,
+    dateTime: String,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = CalendarIcon,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = dateTime,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
     }
 }
 
