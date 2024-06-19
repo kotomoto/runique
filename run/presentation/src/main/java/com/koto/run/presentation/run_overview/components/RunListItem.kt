0ab7b6d7ddc9d3b8127ec506f0c517ef08b71749
jkotor <jkotor@gmail.com>
2024-06-19 23:53:23 +0200

    Finish run item UI

--- run/presentation/src/main/java/com/koto/run/presentation/run_overview/components/RunListItem.kt
+++ run/presentation/src/main/java/com/koto/run/presentation/run_overview/components/RunListItem.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalFoundationApi::class)
+@file:OptIn(ExperimentalFoundationApi::class, ExperimentalLayoutApi::class)
 
 package com.koto.run.presentation.run_overview.components
 
@@ -9,9 +9,12 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,18 +23,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,6 +55,7 @@ import com.koto.run.presentation.run_overview.mapper.toRunUi
 import com.koto.run.presentation.run_overview.model.RunDataUi
 import com.koto.run.presentation.run_overview.model.RunUi
 import java.time.ZonedDateTime
+import kotlin.math.max
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -59,28 +68,49 @@ fun RunListItem(
     var showDropdown by remember {
         mutableStateOf(false)
     }
-    Column(
-        modifier = modifier
-            .clip(RoundedCornerShape(15.dp))
-            .background(MaterialTheme.colorScheme.surface)
-            .combinedClickable(
-                onClick = {},
-                onLongClick = {
-                    showDropdown = true
-                }
+    Box {
+        Column(
+            modifier = modifier
+                .clip(RoundedCornerShape(15.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .combinedClickable(
+                    onClick = {},
+                    onLongClick = {
+                        showDropdown = true
+                    }
+                )
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            MapImage(imageUrl = runUi.mapPictureUrl)
+            RunningTimeSection(
+                modifier = Modifier.fillMaxWidth(),
+                duration = runUi.duration,
             )
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        MapImage(imageUrl = runUi.mapPictureUrl)
-        RunningTimeSection(
-            modifier = Modifier.fillMaxWidth(),
-            duration = runUi.duration,
-        )
-        HorizontalDivider(color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f))
-        RunningDateSection(
-            dateTime = runUi.dateTime,
-        )
+            HorizontalDivider(color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f))
+            RunningDateSection(
+                dateTime = runUi.dateTime,
+            )
+            DataGrid(
+                modifier = Modifier.fillMaxWidth(),
+                run = runUi,
+            )
+        }
+
+        DropdownMenu(
+            expanded = showDropdown,
+            onDismissRequest = { showDropdown = false },
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text = stringResource(id = R.string.delete))
+                },
+                onClick = {
+                    showDropdown = false
+                    onDeleteClick()
+                },
+            )
+        }
     }
 }
 
@@ -190,6 +220,57 @@ private fun RunningDateSection(
             text = dateTime,
             color = MaterialTheme.colorScheme.onSurfaceVariant,
         )
+    }
+}
+
+@Composable
+private fun DataGrid(
+    modifier: Modifier = Modifier,
+    run: RunUi,
+) {
+    val runDataUiList = listOf(
+        RunDataUi(
+            name = stringResource(id = R.string.distance),
+            value = run.distance,
+        ),
+        RunDataUi(
+            name = stringResource(id = R.string.pace),
+            value = run.pace,
+        ),
+        RunDataUi(
+            name = stringResource(id = R.string.avg_speed),
+            value = run.avgSpeed,
+        ),
+        RunDataUi(
+            name = stringResource(id = R.string.max_speed),
+            value = run.maxSpeed,
+        ),
+        RunDataUi(
+            name = stringResource(id = R.string.total_elevation),
+            value = run.totalElevation,
+        ),
+    )
+
+    var maxWidth by remember {
+        mutableIntStateOf(0)
+    }
+    val maxWidthDp = with(LocalDensity.current) { maxWidth.toDp() }
+
+    FlowRow(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        runDataUiList.forEach { run ->
+            DataGridCell(
+                modifier = Modifier
+                    .defaultMinSize(minWidth = maxWidthDp)
+                    .onSizeChanged {
+                        maxWidth = max(maxWidth, it.width)
+                    },
+                runData = run,
+            )
+        }
     }
 }
 
